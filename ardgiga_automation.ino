35039f218daf5369b2460656c2d2a5d7d84a9d6a
Travis Farmer <travis.farmer@outlook.com>
2023-10-01 11:56:40 -0400

    added Modbus switches inputs
    will need to make up a 3.3v level-shifted Modbus RS485 board

--- ardgiga_automation.ino
+++ ardgiga_automation.ino
@@ -3,6 +3,7 @@
 #include <ArduinoJson.h>
 #include <WiFi.h>
 #include "arduino_secrets.h"
+#include <ModbusRTUMaster.h>
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = WSSID;        // your network SSID (name)
 char pass[] = WPSWD;        // your network password (use for WPA, or use as key for WEP)
@@ -13,9 +14,13 @@ int status = WL_IDLE_STATUS;
 #define RELAY_HEAT 32
 #define RELAY_COOL 33
 #define RELAY_FAN 34
-#define RELAY_LIGHTS 22
+#define RELAY_LIGHTS1 22
+#define RELAY_LIGHTS2 23
+#define RELAY_LIGHTS3 24
 
-
+#define MODBUS_DE 2
+#define MODBUS_BAUD 9600
+ModbusRTUMaster modbus(Serial1, MODBUS_DE); // serial port, driver enable pin for rs-485 (optional)
 
 DHTNEW mySensor(48);
 
@@ -38,164 +43,372 @@ bool stateHeating = false;
 bool stateCooling = false;
 bool stateFan = false;
 bool stateCool = false;
+bool stateLights1 = false;
+bool stateLights1SW = false;
 bool state_act_lights1 = false;
+bool stateLights2 = false;
+bool stateLights2SW = false;
+bool state_act_lights2 = false;
+bool stateLights3 = false;
+bool stateLights3SW = false;
+bool state_act_lights3 = false;
 unsigned long coolTimer = 0UL;
-String setLights1 = "";
-String setComp1 = "";
+bool setLights1 = false;
+bool setLights2 = false;
+bool setLights3 = false;
 
-boolean reconnect() {
-  if (client.connect("ArduinoRoom")) {
-    client.subscribe("room/hvac/mode/set");
-    client.subscribe("room/hvac/temperature/set");
-    client.subscribe("room/switch/lights1/set");
+// Modbus
+bool coils[2];
+bool discreteInputs[2];
+uint16_t holdingRegisters[2];
+uint16_t inputRegisters[3];
 
-  }
-  return client.connected();
+boolean reconnect()
+{
+    if (client.connect("ArduinoRoom"))
+    {
+        client.subscribe("room/hvac/mode/set");
+        client.subscribe("room/hvac/temperature/set");
+        client.subscribe("room/switch/lights1/set");
+        client.subscribe("room/switch/lights2/set");
+        client.subscribe("room/switch/lights3/set");
+
+    }
+    return client.connected();
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  String tmpTopic = topic;
-  char tmpStr[length+1];
-  for (int x=0; x<length; x++) {
-    tmpStr[x] = (char)payload[x]; // payload is a stream, so we need to chop it by length.
-  }
-  tmpStr[length] = 0x00; // terminate the char string with a null
+bool switch2Bool(String switch_state)
+{
+    if (switch_state == "ON")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
-  if (tmpTopic == "room/hvac/mode/set") {setMode = tmpStr; }
-  else if (tmpTopic == "room/hvac/temperature/set") { setTemp = atoi(tmpStr); }
-  else if (tmpTopic == "room/switch/lights1/set") { setLights1 = tmpStr; }
+void callback(char* topic, byte* payload, unsigned int length)
+{
+    String tmpTopic = topic;
+    char tmpStr[length+1];
+    for (int x=0; x<length; x++)
+    {
+        tmpStr[x] = (char)payload[x]; // payload is a stream, so we need to chop it by length.
+    }
+    tmpStr[length] = 0x00; // terminate the char string with a null
+
+    if (tmpTopic == "room/hvac/mode/set")
+    {
+        setMode = tmpStr;
+    }
+    else if (tmpTopic == "room/hvac/temperature/set")
+    {
+        setTemp = atoi(tmpStr);
+    }
+    else if (tmpTopic == "room/switch/lights1/set")
+    {
+        setLights1 = switch2Bool(tmpStr);
+    }else if (tmpTopic == "room/switch/lights2/set")
+    {
+        setLights2 = switch2Bool(tmpStr);
+    }else if (tmpTopic == "room/switch/lights3/set")
+    {
+        setLights3 = switch2Bool(tmpStr);
+    }
 
 }
 
-void setup() {
-  client.setServer(server, 1883);
-  client.setCallback(callback);
 
- // check for the WiFi module:
-  if (WiFi.status() == WL_NO_SHIELD) {
-    //Serial.println("Communication with WiFi module failed!");
-    // don't continue
-    while (true);
-  }
 
-  // attempt to connect to Wifi network:
-  while (status != WL_CONNECTED) {
-    //Serial.print("Attempting to connect to SSID: ");
-    //Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-    status = WiFi.begin(ssid, pass);
+void setup()
+{
+    client.setServer(server, 1883);
+    client.setCallback(callback);
 
-    // wait 3 seconds for connection:
-    delay(3000);
-  }
-  delay(1500);
-  lastReconnectAttempt = 0;
+    modbus.begin(MODBUS_BAUD); // baud rate, config (optional)
 
-  for (int i = 22; i <= 34; i++) {
-    pinMode(i, OUTPUT);
-    digitalWrite(i,HIGH);
-  }
+// check for the WiFi module:
+    if (WiFi.status() == WL_NO_SHIELD)
+    {
+        //Serial.println("Communication with WiFi module failed!");
+        // don't continue
+        while (true);
+    }
+
+    // attempt to connect to Wifi network:
+    while (status != WL_CONNECTED)
+    {
+        //Serial.print("Attempting to connect to SSID: ");
+        //Serial.println(ssid);
+        // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+        status = WiFi.begin(ssid, pass);
+
+        // wait 3 seconds for connection:
+        delay(3000);
+    }
+    delay(1500);
+    lastReconnectAttempt = 0;
+
+    for (int i = 22; i <= 34; i++)
+    {
+        pinMode(i, OUTPUT);
+        digitalWrite(i,HIGH);
+    }
 
 }
 
-void loop() {
-  if (!client.connected()) {
-    long now = millis();
-    if (now - lastReconnectAttempt > 5000) {
-      lastReconnectAttempt = now;
-      // Attempt to reconnect
-      if (reconnect()) {
-        lastReconnectAttempt = 0;
-      }
+void loop()
+{
+    if (!client.connected())
+    {
+        long now = millis();
+        if (now - lastReconnectAttempt > 5000)
+        {
+            lastReconnectAttempt = now;
+            // Attempt to reconnect
+            if (reconnect())
+            {
+                lastReconnectAttempt = 0;
+            }
+        }
     }
-  } else {
-    // Client connected
+    else
+    {
+        // Client connected
 
-    client.loop();
-  }
-
-  if (millis() - lastTimer >= 500) {
-    /** \brief setup current values
-     *
-     *
-     */
-    lastTimer = millis();
-    int chk = mySensor.read();
-    humidityRH = mySensor.getHumidity();
-    tempF = ((mySensor.getTemperature() * 1.80) + 32.00);
-
-
-
-    /** \brief handle switches
-     *
-     *
-     */
-    if (setLights1 == "ON") {
-        digitalWrite(RELAY_LIGHTS,HIGH);
-        state_act_lights1 = true;
-    } else if (setLights1 == "OFF") {
-        digitalWrite(RELAY_LIGHTS,LOW);
-        state_act_lights1 = false;
+        client.loop();
     }
 
-    /*if (state_act_lights1 == true) {
-        client.publish("room/switch/lights1","ON");
-    } else {
-        client.publish("room/switch/lights1","OFF");
-    }*/
+    if (millis() - lastTimer >= 500)
+    {
+        /** \brief setup current values
+         *
+         *
+         */
+        lastTimer = millis();
+        int chk = mySensor.read();
+        humidityRH = mySensor.getHumidity();
+        tempF = ((mySensor.getTemperature() * 1.80) + 32.00);
 
-    if (state_act_lights1 == true) {
-        client.publish("room/switch/lights1/state","ON");
-    } else {
-        client.publish("room/switch/lights1/state","OFF");
-    }
+        modbus.readDiscreteInputs(1, 0, discreteInputs, 3);
 
-    /** \brief handle thermostat
-     *
-     *
-     */
-    if (setMode == "off") {
-        stateHeating = false;
-        stateCooling = false;
-    } else if (setMode == "heat") {
-        stateCooling = false;
-        if (tempF >= setTemp) {stateHeating = false;}
-        else if (tempF <= setTemp - thermostatHysteresis) {stateHeating = true;}
-    } else if (setMode == "cool") {
-        stateHeating = false;
-        if (tempF <= setTemp) {stateCooling = false;}
-        else if (tempF >= setTemp + thermostatHysteresis) {stateCooling = true;}
-    } else if (setMode == "auto") {
-        if (tempF >= setTemp + thermostatHysteresis && stateCooling == false) {
-            stateCooling = true;
-            stateHeating = false;
-        } else if (tempF <= setTemp - thermostatHysteresis && stateHeating == false) {
-            stateCooling = false;
-            stateHeating = true;
-        } else if ((tempF >= setTemp && stateHeating == true) || (tempF <= setTemp && stateCooling == true)) {
-            stateCooling = false;
-            stateHeating = false;
+        /** \brief handle switches
+         *
+         *
+         */
+
+        if (setLights1 == true)
+        {
+            stateLights1 = true;
+        }
+        else
+        {
+            stateLights1 = false;
+        }
+        if (discreteInputs[0] == false)
+        {
+            stateLights1SW = true;
+        }
+        else
+        {
+            stateLights1SW = false;
+        }
+        if (stateLights1 == stateLights1SW)
+        {
+            digitalWrite(RELAY_LIGHTS1,HIGH);
+            state_act_lights1 = true;
+        }
+        else
+        {
+            digitalWrite(RELAY_LIGHTS1,LOW);
+            state_act_lights1 = false;
         }
 
+        if (state_act_lights1 == true)
+        {
+            client.publish("room/switch/lights1","ON");
+        }
+        else
+        {
+            client.publish("room/switch/lights1","OFF");
+        }
+
+
+        if (setLights2 == true)
+        {
+            stateLights2 = true;
+        }
+        else
+        {
+            stateLights2 = false;
+        }
+        if (discreteInputs[1] == false)
+        {
+            stateLights2SW = true;
+        }
+        else
+        {
+            stateLights2SW = false;
+        }
+        if (stateLights2 == stateLights2SW)
+        {
+            digitalWrite(RELAY_LIGHTS2,HIGH);
+            state_act_lights2 = true;
+        }
+        else
+        {
+            digitalWrite(RELAY_LIGHTS2,LOW);
+            state_act_lights2 = false;
+        }
+
+        if (state_act_lights2 == true)
+        {
+            client.publish("room/switch/lights2","ON");
+        }
+        else
+        {
+            client.publish("room/switch/lights2","OFF");
+        }
+
+
+        if (setLights3 == true)
+        {
+            stateLights3 = true;
+        }
+        else
+        {
+            stateLights3 = false;
+        }
+        if (discreteInputs[2] == false)
+        {
+            stateLights3SW = true;
+        }
+        else
+        {
+            stateLights3SW = false;
+        }
+        if (stateLights3 == stateLights3SW)
+        {
+            digitalWrite(RELAY_LIGHTS3,HIGH);
+            state_act_lights3 = true;
+        }
+        else
+        {
+            digitalWrite(RELAY_LIGHTS3,LOW);
+            state_act_lights3 = false;
+        }
+
+        if (state_act_lights3 == true)
+        {
+            client.publish("room/switch/lights3","ON");
+        }
+        else
+        {
+            client.publish("room/switch/lights3","OFF");
+        }
+
+        /** \brief handle thermostat
+         *
+         *
+         */
+        if (setMode == "off")
+        {
+            stateHeating = false;
+            stateCooling = false;
+        }
+        else if (setMode == "heat")
+        {
+            stateCooling = false;
+            if (tempF >= setTemp)
+            {
+                stateHeating = false;
+            }
+            else if (tempF <= setTemp - thermostatHysteresis)
+            {
+                stateHeating = true;
+            }
+        }
+        else if (setMode == "cool")
+        {
+            stateHeating = false;
+            if (tempF <= setTemp)
+            {
+                stateCooling = false;
+            }
+            else if (tempF >= setTemp + thermostatHysteresis)
+            {
+                stateCooling = true;
+            }
+        }
+        else if (setMode == "auto")
+        {
+            if (tempF >= setTemp + thermostatHysteresis && stateCooling == false)
+            {
+                stateCooling = true;
+                stateHeating = false;
+            }
+            else if (tempF <= setTemp - thermostatHysteresis && stateHeating == false)
+            {
+                stateCooling = false;
+                stateHeating = true;
+            }
+            else if ((tempF >= setTemp && stateHeating == true) || (tempF <= setTemp && stateCooling == true))
+            {
+                stateCooling = false;
+                stateHeating = false;
+            }
+
+        }
+        if (stateHeating == true)
+        {
+            digitalWrite(RELAY_HEAT, LOW);
+        }
+        else
+        {
+            digitalWrite(RELAY_HEAT, HIGH);
+        }
+        if (stateCooling == true && stateFan == false && stateCool == false)
+        {
+            digitalWrite(RELAY_FAN, LOW);
+            stateFan = true;
+        }
+        else if (stateCooling == true && millis() - coolTimer >= 30000 && stateFan == true && stateCool == false)
+        {
+            stateCool = true;
+            coolTimer = millis();
+            digitalWrite(RELAY_COOL, LOW);
+        }
+        else if(stateCooling == false)
+        {
+            digitalWrite(RELAY_COOL, HIGH);
+            digitalWrite(RELAY_FAN, HIGH);
+            stateFan = false;
+            stateCool = false;
+        }
+
+        char sz[32];
+        String strAction = "";
+        if (stateHeating == true)
+        {
+            strAction = "heating";
+        }
+        else if (stateCooling == true)
+        {
+            strAction = "cooling";
+        }
+        else
+        {
+            strAction = "off";
+        }
+        strAction.toCharArray(sz, 32);
+        client.publish("room/hvac/action",sz);
+        sprintf(sz, "%6.2f", tempF);
+        client.publish("room/hvac/temperature/current",sz);
+        sprintf(sz, "%6.2f", humidityRH);
+        client.publish("room/hvac/humidity/current",sz);
+
+
     }
-    if (stateHeating == true) {digitalWrite(RELAY_HEAT, LOW);}
-    else {digitalWrite(RELAY_HEAT, HIGH);}
-    if (stateCooling == true && stateFan == false && stateCool == false) { digitalWrite(RELAY_FAN, LOW); stateFan = true; }
-    else if (stateCooling == true && millis() - coolTimer >= 30000 && stateFan == true && stateCool == false) {stateCool = true; coolTimer = millis(); digitalWrite(RELAY_COOL, LOW);}
-    else if(stateCooling == false) {digitalWrite(RELAY_COOL, HIGH); digitalWrite(RELAY_FAN, HIGH); stateFan = false; stateCool = false;}
-
-    char sz[32];
-    String strAction = "";
-    if (stateHeating == true) {strAction = "heating";}
-    else if (stateCooling == true) {strAction = "cooling";}
-    else {strAction = "off";}
-    strAction.toCharArray(sz, 32);
-    client.publish("room/hvac/action",sz);
-    sprintf(sz, "%6.2f", tempF);
-    client.publish("room/hvac/temperature/current",sz);
-    sprintf(sz, "%6.2f", humidityRH);
-    client.publish("room/hvac/humidity/current",sz);
-
-
-  }
 }
